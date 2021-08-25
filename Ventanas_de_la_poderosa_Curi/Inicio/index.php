106305a953c42831f91335974fec1ca8570c899b
bre23a <brenda_hdzg@hotmail.com>
2021-08-25 12:47:51 -0500

    Barra_Principal_sin_intento_boostrap_lupa_tiene_un_problemita

--- Ventanas_de_la_poderosa_Curi/Inicio/index.php
+++ Ventanas_de_la_poderosa_Curi/Inicio/index.php
@@ -13,14 +13,19 @@
 <body>
 <section class="home">
         <div class="in-flex"> 
-            <div class="Botones">
-                
-                <h3 class="hal">Iniciar sesion </h3>
+            <div class="Botones"> 
+            <img src="/CaesMal/Cubbi_BDM_PWCI/Ventanas_de_la_poderosa_Curi/Multimedia/more.png" alt="more" class="more"> 
+                <img src="/CaesMal/Cubbi_BDM_PWCI/Ventanas_de_la_poderosa_Curi/Multimedia/logo.png" alt="logo"> 
+                <div class="buscador">                   
+                <input type="search">
+                <img src="/CaesMal/Cubbi_BDM_PWCI/Ventanas_de_la_poderosa_Curi/Multimedia/lupa.png" alt="" class="lupa">
+                </div>             
+
+                <h3 class="botoncito">Iniciar sesion </h3>
             </div>
         </div>
         <div class="in-flex">
-            <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos dolores, illum, quod magnam repudiandae sequi, odio accusantium debitis vero fuga labore porro velit sapiente. In pariatur iusto assumenda modi temporibus?            </div>
-            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet unde magni pariatur beatae at perferendis suscipit molestiae repellat laboriosam quas, possimus similique enim odit nihil atque odio doloribus exercitationem eos.</div>
+            
         </div>
     </section>
     <section>
@@ -32,21 +37,21 @@
             </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                <img src="/Multimedia/4fe3a44831066043398fe4f9388daa7d.gif" class="d-block w-100" alt="...">
+                <img src="/CaesMal/Cubbi_BDM_PWCI/Ventanas_de_la_poderosa_Curi/Multimedia/a.gif" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Bordado</h5>
                     <p>Impartida por Brenda</p>
                     </div>
             </div>
             <div class="carousel-item">
-                <img src="/Multimedia/5ef1f22a72d06f33d3b93645964a905b.gif" class="d-block w-100" alt="...">
+                <img src="/CaesMal/Cubbi_BDM_PWCI/Ventanas_de_la_poderosa_Curi/Multimedia/b.gif" class="d-block w-100" alt="..."  >
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Croche</h5>
                     <p>Impartida por curi</p>
                 </div>
                 </div>
             <div class="carousel-item">
-                <img src="/Multimedia/6e782a5ae0b44f52bf867f361e73c26a.gif" class="d-block w-100" alt="...">
+                <img src="/CaesMal/Cubbi_BDM_PWCI/Ventanas_de_la_poderosa_Curi/Multimedia/c.gif" class="d-block w-100" alt="..." >   
                 <div class="carousel-caption d-none d-md-block">
                     <h5>Evadir impuestos</h5>
                     <p>Impartida por anonimo</p>
@@ -64,5 +69,6 @@
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
         </div>
     </section>
+
 </body>
 </html>
