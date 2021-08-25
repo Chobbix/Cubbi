c4cfa889e8614c162d7c0076f4b5eab2b2292964
bre23a <brenda_hdzg@hotmail.com>
2021-08-25 11:24:12 -0500

    carrusel

---
+++ Ventanas_de_la_poderosa_Curi/Inicio/index.php
@@ -0,0 +1,68 @@
+<!--Hola, soy un archivo PHP creado por Victor. Provecho Brenda-->
+<!--Que amable Victor-->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="Pinicial.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <title>Document</title>
+    </head>
+<body>
+<section class="home">
+        <div class="in-flex"> 
+            <div class="Botones">
+                
+                <h3 class="hal">Iniciar sesion </h3>
+            </div>
+        </div>
+        <div class="in-flex">
+            <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos dolores, illum, quod magnam repudiandae sequi, odio accusantium debitis vero fuga labore porro velit sapiente. In pariatur iusto assumenda modi temporibus?            </div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet unde magni pariatur beatae at perferendis suscipit molestiae repellat laboriosam quas, possimus similique enim odit nihil atque odio doloribus exercitationem eos.</div>
+        </div>
+    </section>
+    <section>
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="/Multimedia/4fe3a44831066043398fe4f9388daa7d.gif" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Bordado</h5>
+                    <p>Impartida por Brenda</p>
+                    </div>
+            </div>
+            <div class="carousel-item">
+                <img src="/Multimedia/5ef1f22a72d06f33d3b93645964a905b.gif" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Croche</h5>
+                    <p>Impartida por curi</p>
+                </div>
+                </div>
+            <div class="carousel-item">
+                <img src="/Multimedia/6e782a5ae0b44f52bf867f361e73c26a.gif" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Evadir impuestos</h5>
+                    <p>Impartida por anonimo</p>
+                </div>
+            </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+        </div>
+    </section>
+</body>
+</html>
