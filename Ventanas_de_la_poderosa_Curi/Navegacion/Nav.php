8a7eb5afc4bfe35627142c496a5bc02f496b11e0
bre23a <brenda_hdzg@hotmail.com>
2021-09-01 09:50:09 -0500

    Hice-una-pagina-para-el-nav-lo-guardo-porque-siento-que-boostrap-va-a-cagar-vivia-jenny-rivera

---
+++ Ventanas_de_la_poderosa_Curi/Navegacion/Nav.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="nav.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <nav class="nav">
+            <img src="/Cubbi_BDM_PWCI/Ventanas_de_la_poderosa_Curi/Multimedia/logo.png" alt="logo" class="logo"> 
+            <ul class="nav-menu">
+                <li><a href="#" class="nav-item  buscador" > <input type="search" class="search"> </li>
+                <li><a href="#" class="nav-item buscador"> <img src="/Cubbi_BDM_PWCI/Ventanas_de_la_poderosa_Curi/Multimedia/lupa.png" alt="" class="lupa buscador"></a></li>
+                <li><a href="#" class="nav-item"><img src="/Cubbi_BDM_PWCI/Ventanas_de_la_poderosa_Curi/Multimedia/carrito-de-compras.png  " alt="" class="carrito"></a></li>
+                <h1 class="separacion">|</h1>
+                <li><a href="#" class="nav-item">Iniciar sesion</a></li>
+            </ul>
+        </nav>
+    </header>
+
+</body>
+</html>
