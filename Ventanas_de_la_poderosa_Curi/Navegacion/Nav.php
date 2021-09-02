dfe5d73962ccf3edf35fa088ee839089868336c1
bre23a <brenda_hdzg@hotmail.com>
2021-09-02 12:38:49 -0500

    ajajai-ya-ando-ready-con-el-menu

--- Ventanas_de_la_poderosa_Curi/Navegacion/Nav.php
+++ Ventanas_de_la_poderosa_Curi/Navegacion/Nav.php
@@ -4,19 +4,52 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/5554cc4983.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
     <link rel="stylesheet" href="nav.css">
+    <link rel="stylesheet" href="nav.js">
     <title>Document</title>
 </head>
 <body>
     <header class="header">
-        <nav class="nav">
-            <img src="/Cubbi_BDM_PWCI/Ventanas_de_la_poderosa_Curi/Multimedia/logo.png" alt="logo" class="logo"> 
-            <ul class="nav-menu">
-                <li><a href="#" class="nav-item  buscador" > <input type="search" class="search"> </li>
-                <li><a href="#" class="nav-item buscador"> <img src="/Cubbi_BDM_PWCI/Ventanas_de_la_poderosa_Curi/Multimedia/lupa.png" alt="" class="lupa buscador"></a></li>
-                <li><a href="#" class="nav-item"><img src="/Cubbi_BDM_PWCI/Ventanas_de_la_poderosa_Curi/Multimedia/carrito-de-compras.png  " alt="" class="carrito"></a></li>
-                <h1 class="separacion">|</h1>
-                <li><a href="#" class="nav-item">Iniciar sesion</a></li>
+        <nav class="nav">           
+            <a href="#"> <img src="/Cubbi_BDM_PWCI/Ventanas_de_la_poderosa_Curi/Multimedia/logo.png" alt="logo" class="logo">  </a>   
+            <ul class="nav-menu">                 
+                <ul class="menu-categorias">
+                    <li><a href="#" >Cursos <i class="fas fa-angle-down"></i></a>                    
+                        <ul class="longitud">
+                            <li><a href="#" >Categorias </a>                            
+                                <ul class="diseno">
+                                    <scroll-container>
+                                        <li><a href="#">Azucar</a></li>
+                                        <li><a href="#">leche</a></li>
+                                        <li><a href="#">huevos</a></li>
+                                        <li><a href="#">Azucar</a></li>                                                                                     
+                                    </scroll-container>
+                                    
+                                </ul>
+                            </li>
+                            <li class="a"><a href="#">Lo mas nuevo </a>
+                                <ul class="diseno">
+                                    <scroll-container>
+                                        <li><a href="#">dinero</a></li>
+                                        <li><a href="#">dinero</a></li>
+                                        <li><a href="#">dinero</a></li>
+                                        <li><a href="#">dinero</a></li>                                                                                     
+                                    </scroll-container>
+                                </ul>                                
+                            </li>                                
+                        </ul>
+                    </li>
+                    <li><a href="#">Maestros</a></li>
+                </ul>               
+                <li><a href="#" class="nav-item buscador " > <input type="text"  id="inputsearch" placeholder="¿Que vas a aprender?"class="search"> </li>
+                <li><a href="#" class="nav-item buscador "> <i class="fas fa-search"></i></a></li>
+                <li><a href="#" class="nav-item"> <button class="btn-carrito "> <i class="fas fa-shopping-cart" class=""></i></button> </a></li>
+                <li><a href="#" class="nav-item"> <button class="btn-sesion"> Iniciar sesion </button> </a></li>
+                <li><a href="#" class="nav-item"> <button class="btn-sesion btn-log "> Prueba gratis </button> </a></li>
             </ul>
         </nav>
     </header>
