48cc4a92688d27729ef005f45cd7d366b211f8a3
bre23a <brenda_hdzg@hotmail.com>
2021-09-03 01:45:45 -0500

    diseno-de-la-ventana-de-login-lista

---
+++ Ventanas_de_la_poderosa_Curi/Login/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css"> 
+    <link rel="stylesheet" href="/Cubbi_BDM_PWCI/Ventanas_de_la_poderosa_Curi/Navegacion/nav.css"> 
+<title>Document</title>
+</head>
+<body>
+    <div class="div">
+    <?php include ('D:\wamp\www\Cubbi_BDM_PWCI\Ventanas_de_la_poderosa_Curi\Navegacion/Nav.php');?>
+    </div>
+    <div class="contenedor">
+        <div class="contenido">
+            <div class="tittle "><h1> Iniciar sesion</h1></div>
+            <div class="cont-texto">
+                <div class="email cont"> 
+                    <h1>Correo</h1>
+                    <input type="text"  id="inputsearch" placeholder="Correo" class="diseno-input"> 
+                </div>
+                <div class="pass cont">
+                    <h1>Contraseña</h1>
+                    <input type="password"  id="inputsearch" placeholder="Contraseña" class="diseno-input"> 
+                </div>    
+                <button class="btn-inicio"> Iniciar sesion</button>   
+                <div class="registrate">
+                    <h3 class="text-registro">¿Como?¿No tienes cuenta? </h3> <a href="#" > <h4 class="text-registro link">Registrate</h4></a>
+                </div>    
+            </div>
+        </div>
+    
+    </div>
+    <footer> soy un footer</footer>
+
+</body>
+</html>    
--- Ventanas_de_la_poderosa_Curi/Navegacion/Nav.php
+++ Ventanas_de_la_poderosa_Curi/Navegacion/Nav.php
@@ -6,10 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/5554cc4983.js" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="nav.css">
-    <link rel="stylesheet" href="nav.js">
     <title>Document</title>
 </head>
 <body>
@@ -53,6 +51,7 @@
             </ul>
         </nav>
     </header>
+   
 
 </body>
 </html>
