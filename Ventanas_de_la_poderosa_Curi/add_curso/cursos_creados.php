32c123c8aa7db0b2e02d7ac0e933adee530947e0
Chobbi <victor.perezml@uanl.edu.mx>
2021-11-08 04:01:17 -0600

    pantalla load separada por partes

---
+++ Ventanas_de_la_poderosa_Curi/add_curso/cursos_creados.php
@@ -0,0 +1,42 @@
+<div class="contenedor-diplomas bloque_contenedor_cursos" id="Registro">
+    <div class="cont_cursos">
+        <div class="bloque_de_cursos">
+            <div class="curso_cajita">
+                <div class="info_basica_curso">
+                    <h1>*Titulo del curso*</h1>
+                    <h1>*borrador*</h1>
+                </div>
+                <div class="btn_edicion">
+                    <div class="">
+                        <button class="ds_delete">
+                            <label for="cursos">
+                                <h1>Editar</h1>
+                            </label>
+                        </button>
+                    </div>
+                    <button class="ds_delete">
+                        <h1>Eliminar</h1>
+                    </button>
+                </div>
+            </div>
+            <div class="curso_cajita">
+                <div class="info_basica_curso">
+                    <h1>*Titulo del curso*</h1>
+                    <h1>*Publicado*</h1>
+                </div>
+                <div class="btn_edicion">
+                    <div class="">
+                        <button class="ds_delete">
+                            <label for="cursos">
+                                <h1>Editar</h1>
+                            </label>
+                        </button>
+                    </div>
+                    <button class="ds_delete">
+                        <h1>Eliminar</h1>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
